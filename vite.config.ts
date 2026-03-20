a122ebd2d89fc6ca007dd5a1028c7152b0e44f86
최희원 <jeeen1@naver.com>
2026-03-20 23:36:04 +0900

    fix ci

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from "@vitejs/plugin-react";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
-export default defineConfig(() => ({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/tharsis/" : "/",
   plugins: [
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
     react(),
